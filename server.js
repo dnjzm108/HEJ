ddd91b0967aa9dcbc951ffc157de1ec26c13149d
dnjzm108 <dnjzm108@kakao.com>
2021-06-07 17:35:38 +0900

    123

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const router = require('./routers/index');
 const {board,information,user,sequelize} = require('./models');
 
+
 sequelize.sync({force:false})
 .then(()=>{
     console.log('접속이 완료 되었습니다');
@@ -14,18 +15,13 @@ sequelize.sync({force:false})
     console.log('접속 에러');
 })
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:false}));
 app.set('view engine','html');
 nunjucks.configure('views',{
     express:app
 })
 app.use(express.static('public'));
 
-app.get('/board', async (req,res)=>{
-    let result = await board.findAll();
-    console.log(result);
-    res.json({result});
-})
 
 app.use('/',router)
 
