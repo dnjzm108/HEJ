1aba4c9fcfe0847f63e5ab8812ad26a0ead61486
heini <melme900@gmail.com>
2021-06-22 17:04:12 +0900

    dd

--- pagination.js
+++ pagination.js
@@ -7,6 +7,23 @@ async function pagination(pagin) {
     let page_hired = [];
     let resultall;
     let result;
+
+    if(pagin.localUrl == 'undefined'){
+        resultsall = await search[pagin.table].findAll({});
+        result = await search[pagin.table].findAll({
+            raw: true,
+            limit: 9,
+            order: [['id', 'DESC']],
+            offset: offset,
+        });
+        let totalrecord = resultsall.length;
+        let total_page = Math.ceil(totalrecord / 9);
+        for (i = 1; i <= total_page; i++) {
+            page_hired.push(i);
+        };
+        return {page_hired,result,totalrecord,offset};
+    }
+
     if(pagin.localUrl != undefined){
         resultsall = await search[pagin.table].findAll({ where: { type: `${pagin.localUrl}` } })
         result = await search[pagin.table].findAll({
@@ -30,7 +47,6 @@ async function pagination(pagin) {
     for (i = 1; i <= total_page; i++) {
         page_hired.push(i);
     };
-
 
     return {page_hired,result,totalrecord,offset}
 }
