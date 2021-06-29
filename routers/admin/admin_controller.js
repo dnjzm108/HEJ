8e45676e34cc8605c12b2b87bcf5e90c5c06321d
heini <melme900@gmail.com>
2021-06-29 16:59:05 +0900

    dd

--- routers/admin/admin_controller.js
+++ routers/admin/admin_controller.js
@@ -72,8 +72,8 @@ let upload_success = async (req, res) => {
 
     if (localUrl == 'hired') {
         let thumbnail = req.file == undefined ? '' : `/uploads/user_image/${req.file.filename}`;
-        let { title, content, writer, type } = req.body;
-        await search[localUrl].create({title,content,writer,type, thumbnail});
+        let { title, content, writer, type, creator } = req.body;
+        await search[localUrl].create({title,content,writer,type, thumbnail, creator});
         res.redirect(`/admin/hired/${type}`);
         return;
     }
